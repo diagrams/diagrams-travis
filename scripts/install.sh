9a91a69bcd8f299c821b83e094a4e6d2e29ff537
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-06 22:11:34 -0500

    invert test and remove debugging statements

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,14 +5,12 @@ $CABAL install haddock -j$NUM_CPU
 
 echo "Installing HEAD dependencies: $HEAD_DEPS"
 # install any dependencies checked out from git
-if [[ -z "$HEAD_DEPS" ]]
+if ! [[ -z "$HEAD_DEPS" ]]
   then
-    echo "HEAD_DEPS is non-null"
     for DEP in $HEAD_DEPS
     do
       DIRS="$DIRS $DEP/"
     done
-    echo $DIRS
     echo "$CABAL install $DIRS"
     $CABAL install $DIRS
 fi
