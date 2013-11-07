8381c8a3bbeaabc2e130c02dcd545780fd4518ef
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-06 21:35:34 -0500

    checkout and install HEAD_DEPS from github

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,3 +4,11 @@
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER
+
+for DEP in $HEAD_DEPS
+do
+  git clone --quiet git://github.com/diagrams/$DEP.git
+  cd $DEP
+  if git branch -a |grep -x "  remotes/origin/${TRAVIS_BRANCH}" > /dev/null; then git checkout ${TRAVIS_BRANCH}; fi
+  cd ..
+done
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 $CABAL update
+
+# install any dependencies checked out from git
+if [[ -z "$HEAD_DEPS" ]]
+  then
+    for DEP in $HEAD_DEPS
+    do
+      DIRS="$DIRS $DEP/"
+    done
+    $CABAL install $DIRS
+fi
+
 $CABAL install --enable-tests --enable-benchmarks --only-dependencies -j$NUM_CPU
