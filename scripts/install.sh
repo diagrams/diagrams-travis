0b78f5b76bd31a68817f1f9f2acae5e6b9e976c6
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-06 22:02:35 -0500

    some debugging echo statements

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,17 @@
 $CABAL update
 $CABAL install haddock -j$NUM_CPU
 
+echo "Installing HEAD dependencies: $HEAD_DEPS"
 # install any dependencies checked out from git
 if [[ -z "$HEAD_DEPS" ]]
   then
+    echo "HEAD_DEPS is non-null"
     for DEP in $HEAD_DEPS
     do
       DIRS="$DIRS $DEP/"
     done
+    echo $DIRS
+    echo "$CABAL install $DIRS"
     $CABAL install $DIRS
 fi
 
