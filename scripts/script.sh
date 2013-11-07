78066fccaf6177d8eb173e69ccc9fb501afa82cb
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-06 21:52:45 -0500

    sequence script components with &&

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 
-$CABAL configure --enable-tests --enable-benchmarks -v2
-$CABAL build --ghc-options='-Wall -Werror'
-$CABAL test
-$CABAL haddock
-$CABAL check
-$CABAL sdist
+$CABAL configure --enable-tests --enable-benchmarks -v2 && $CABAL build --ghc-options='-Wall -Werror' && $CABAL test && $CABAL haddock && $CABAL check && $CABAL sdist
