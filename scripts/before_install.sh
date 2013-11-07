1176812cf99f8dec3c7ae40982d9c4eea7da5a72
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-07 07:07:45 -0500

    install packages listed in $UBUNTU_PKGS

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,12 @@ sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER
 
+if ! [[ -z "$UBUNTU_PKGS" ]]
+  then
+    echo "Installing Ubuntu packages: $UBUNTU_PKGS"
+    sudo apt-get install $UBUNTU_PKGS
+fi
+
 for DEP in $HEAD_DEPS
 do
   echo "Cloning $DEP from github..."
