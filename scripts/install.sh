96918173e376bc76ffacd7bbdcc8ecc3d955d01e
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-08 15:32:52 -0500

    install: chain with && so the build will fail if an individual component does

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
-$CABAL update
-if ! [[ $GHCVER == "head" ]]
-  then
-    $CABAL install haddock -j$NUM_CPU
-fi
-
-if ! [[ -z "$EXTRA_DEPS_PRE" ]]
-  then
-    echo "Pre-installing extra dependencies: $EXTRA_DEPS_PRE"
-    $CABAL install $EXTRA_DEPS_PRE -j$NUM_CPU
-fi
-
-# install any dependencies checked out from git
-if ! [[ -z "$HEAD_DEPS" ]]
-  then
-    echo "Installing HEAD dependencies: $HEAD_DEPS"
-    for DEP in $HEAD_DEPS
-    do
-      DIRS="$DIRS $DEP/"
-    done
-    echo "$CABAL install $DIRS -j$NUM_CPU"
-    $CABAL install $DIRS -j$NUM_CPU
-fi
-
-if ! [[ -z "$EXTRA_DEPS" ]]
-  then
-    echo "Installing extra dependencies: $EXTRA_DEPS"
-    $CABAL install $EXTRA_DEPS -j$NUM_CPU
-fi
-
 INSTALL_CMD="$CABAL install --enable-tests --enable-benchmarks --only-dependencies -j$NUM_CPU"
-echo $INSTALL_CMD
-$INSTALL_CMD --dry-run
-$INSTALL_CMD
+
+$CABAL update\
+  && if ! [[ $GHCVER == "head" ]]
+       then
+         $CABAL install haddock -j$NUM_CPU
+     fi\
+  && if ! [[ -z "$EXTRA_DEPS_PRE" ]]
+       then
+         echo "Pre-installing extra dependencies: $EXTRA_DEPS_PRE"
+         $CABAL install $EXTRA_DEPS_PRE -j$NUM_CPU
+     fi\
+  && if ! [[ -z "$HEAD_DEPS" ]]
+       then
+         echo "Installing HEAD dependencies: $HEAD_DEPS"
+         for DEP in $HEAD_DEPS
+         do
+           DIRS="$DIRS $DEP/"
+         done
+         echo "$CABAL install $DIRS -j$NUM_CPU"
+         $CABAL install $DIRS -j$NUM_CPU
+     fi\
+  && if ! [[ -z "$EXTRA_DEPS" ]]
+       then
+         echo "Installing extra dependencies: $EXTRA_DEPS"
+         $CABAL install $EXTRA_DEPS -j$NUM_CPU
+     fi\
+  && echo $INSTALL_CMD\
+  && $INSTALL_CMD --dry-run\
+  && $INSTALL_CMD
