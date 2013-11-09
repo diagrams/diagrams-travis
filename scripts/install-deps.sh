d1b8c19a6eb914f38dee2e20360c97c02d6a6a27
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-09 15:54:09 -0500

    split out install into install-deps and install
    This way e.g. diagrams-doc can just use the install-deps part.

---
+++ scripts/install-deps.sh
@@ -0,0 +1,36 @@
+$CABAL update\
+  && if ! [[ $GHCVER == "head" ]]
+       then
+         $CABAL install haddock -j$NUM_CPU
+     fi\
+  && if ! [[ -z "$EXTRA_DEPS_PRE" ]]
+       then
+         echo "============================================================"
+         echo "Pre-installing extra dependencies: $EXTRA_DEPS_PRE"
+	 EXTRA_DEPS_PRE_INSTALL="$CABAL install $EXTRA_DEPS_PRE $CABAL_CONSTRAINTS -j$NUM_CPU"
+	 echo $EXTRA_DEPS_PRE_INSTALL
+	 $EXTRA_DEPS_PRE_INSTALL --dry-run -v3
+	 $EXTRA_DEPS_PRE_INSTALL
+     fi\
+  && if ! [[ -z "$HEAD_DEPS" ]]
+       then
+         echo "============================================================"
+         echo "Installing HEAD dependencies: $HEAD_DEPS"
+         for DEP in $HEAD_DEPS
+         do
+           DIRS="$DIRS $DEP/"
+         done
+         HEAD_DEPS_INSTALL="$CABAL install $DIRS $CABAL_CONSTRAINTS -j$NUM_CPU"
+	 echo $HEAD_DEPS_INSTALL
+	 $HEAD_DEPS_INSTALL --dry-run -v3
+         $HEAD_DEPS_INSTALL
+     fi\
+  && if ! [[ -z "$EXTRA_DEPS" ]]
+       then
+         echo "============================================================"
+         echo "Installing extra dependencies: $EXTRA_DEPS"
+	 EXTRA_DEPS_INSTALL="$CABAL install $EXTRA_DEPS $CABAL_CONSTRAINTS -j$NUM_CPU"
+	 echo $EXTRA_DEPS_INSTALL
+	 $EXTRA_DEPS_INSTALL --dry-run -v3
+	 $EXTRA_DEPS_INSTALL
+     fi\
