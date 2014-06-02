9c213d7ad60b797e643fd3567df22435ba1f8147
Daniel Bergey <bergey@alum.mit.edu>
2014-06-02 00:16:11 +0000

    install test dependencies after HEAD_DEPS

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ INSTALL_CMD="$CABAL install --enable-tests --enable-benchmarks --only-dependenci
   # && echo "============================================================"\
   # && $CABAL install $CABAL_FLAGS $CABAL_CONSTRAINTS -j$NUM_CPU packunused packdeps\
   # && echo "============================================================"\
-  # && echo "Installing"\
-  # && echo $INSTALL_CMD\
-  # && $INSTALL_CMD --dry-run -v3\
-  # && $INSTALL_CMD
+  && echo "Installing test dependencies"\
+  && echo $INSTALL_CMD\
+  && $INSTALL_CMD --dry-run -v3\
+  && $INSTALL_CMD
