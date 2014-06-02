18f2edd34864f89711fb21a0f17e8822f556050f
Daniel Bergey <bergey@alum.mit.edu>
2014-06-02 00:27:32 +0000

    comments between && still don't work

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,6 @@
 INSTALL_CMD="$CABAL install --enable-tests --enable-benchmarks --only-dependencies $CABAL_FLAGS $CABAL_CONSTRAINTS -j$NUM_CPU"
 
 ./travis/scripts/install-deps.sh\
-  # && echo "============================================================"\
-  # && $CABAL install $CABAL_FLAGS $CABAL_CONSTRAINTS -j$NUM_CPU packunused packdeps\
-  # && echo "============================================================"\
   && echo "Installing test dependencies"\
   && echo $INSTALL_CMD\
   && $INSTALL_CMD --dry-run -v3\
