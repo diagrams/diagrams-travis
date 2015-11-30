81989a0f5f7c15fd008bbcdf53c7d04bd4f453ec
Daniel Bergey <bergey@teallabs.org>
2015-11-29 22:11:13 -0500

    commit git_deps from before_install.sh
    Containerized builds on travis specify the PPA and GHC version in
    travis.yml, instead of calling apt-get.  They still need to clone
    HEAD_DEPS from git.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,17 +9,9 @@ sudo add-apt-repository -y ppa:hvr/ghc\
 	 echo "Installing Ubuntu packages: $UBUNTU_PKGS"
 	 sudo apt-get install $UBUNTU_PKGS
      fi\
-  && HEAD_BRANCH=$(python travis/scripts/head_branch.py)\
-  && for DEP in $HEAD_DEPS
-     do
-       echo "Cloning $DEP from github..."
-       git clone --quiet git://github.com/diagrams/$DEP.git
-       cd $DEP
-       if git branch -a |grep -x "  remotes/origin/${HEAD_BRANCH}" > /dev/null; then git checkout ${HEAD_BRANCH}; fi
-       cd ..
-     done
+  && travis/scripts/git_deps.sh
 
-  # Uncomment whenever hackage is down --- doesn't actually work
+ # Uncomment whenever hackage is down --- doesn't actually work
   # because travis does its own 'cabal update' before even calling our
   # scripts =(
 
