8212ec514eae94123b71c99343dcb122756ddc79
Brent Yorgey <byorgey@gmail.com>
2016-09-10 13:53:09 -0500

    allow skipping cabal test with env var

--- scripts/script.sh
+++ scripts/script.sh
@@ -7,9 +7,16 @@ if [[ $GHCVER == "head" || -n "$SKIP_HADDOCK" ]]
     DOCBUILD="$CABAL haddock"
 fi
 
+if [[ -n "$SKIP_TESTS" ]]
+  then
+    TESTBUILD="echo 'Skipping tests...'"
+  else
+    TESTBUILD="$CABAL test"
+fi
+
 $CABAL configure --enable-tests --enable-benchmarks -v2 $CABAL_FLAGS\
   && $CABAL build --ghc-options='-Wall -Werror -ddump-minimal-imports'\
-  && $CABAL test\
+  && $TESTBUILD\
   && $DOCBUILD\
   && $CABAL check\
   && $CABAL sdist
